4cff754c8f2c7f3797d8890e99746df14efb2616
Gioele795 <gioele.romano@itiscuneo.edu.it>
2026-02-11 11:06:15 +0100

    servo

--- codice/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMovimentoOpMode.java
+++ codice/FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMovimentoOpMode.java
@@ -122,11 +122,6 @@ public class TestMovimentoOpMode extends LinearOpMode {
             double backRightPower  = axial + lateral - yaw;
 
 
-            //Maps the values so they don't exceed 1
-            frontLeftPower = mapFunction(frontLeftPower, 3, 1);
-            frontRightPower = mapFunction(frontRightPower, 3, 1);
-            backLeftPower = mapFunction(backLeftPower, 3, 1);
-            backRightPower = mapFunction(backRightPower, 3, 1);
 
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.
@@ -171,12 +166,6 @@ public class TestMovimentoOpMode extends LinearOpMode {
             telemetry.update();
         }
 
-    }
-    private double mapFunction(double valore, double maxAttuale, double maxRicavare){
-        double ret;
-
-        ret = (valore * maxRicavare) / maxAttuale;
-        return ret;
     }
 
 }
